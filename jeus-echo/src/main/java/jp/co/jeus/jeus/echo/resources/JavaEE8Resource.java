6d731442111e7be443f5b49b0a1dcbd88eefc4e1
sambunarayan <soyou30257@gmail.com>
2021-09-23 17:01:17 +0900

    Edit Response value (hostname)

--- jeus-echo/src/main/java/jp/co/jeus/jeus/echo/resources/JavaEE8Resource.java
+++ jeus-echo/src/main/java/jp/co/jeus/jeus/echo/resources/JavaEE8Resource.java
@@ -1,5 +1,7 @@
 package jp.co.jeus.jeus.echo.resources;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -13,7 +15,15 @@ public class JavaEE8Resource {
 
     @GET
     public Response ping() {
-        String env = System.getenv().get("hostname");
+        String env = "";
+        env += System.getenv().get("hostname");
+        InetAddress addr;
+        try {
+            addr = InetAddress.getLocalHost();
+            env += " | " + addr.getHostAddress();
+        } catch (UnknownHostException ex) {
+            env = ex.getMessage();
+        }
         return Response
                 .ok(env)
                 .build();
