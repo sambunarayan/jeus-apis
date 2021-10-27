4afabe8a87f5a1d4798e026f4727e4b72372f3f3
sambunarayan <soyou30257@gmail.com>
2021-10-27 21:24:09 +0900

    20211027 SpringBoot
    ・Added Lombok

---
+++ jeus-todo/src/test/java/jp/co/jeus/web/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package jp.co.jeus.web;
+
+import jp.co.jeus.web.dto.HelloResponseDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void lombok_test() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
