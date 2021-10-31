f3792217de7c4c5ceec800b3b94dc4c98a2abf9c
sambunarayan <soyou30257@gmail.com>
2021-10-31 20:13:41 +0900

    20211031 SpringBoot
    ・Using JPA

---
+++ jeus-todo/src/main/java/jp/co/jeus/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package jp.co.jeus.web.dto;
+
+import jp.co.jeus.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
