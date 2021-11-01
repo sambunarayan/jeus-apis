3354a62a4706c12b3a11bbd04c14a3d3a1f869e8
sambunarayan <soyou30257@gmail.com>
2021-11-01 22:51:38 +0900

    20211101 SpringBoot
    ・JPA Auditing

--- jeus-todo/src/main/java/jp/co/jeus/domain/posts/Posts.java
+++ jeus-todo/src/main/java/jp/co/jeus/domain/posts/Posts.java
@@ -1,6 +1,6 @@
 package jp.co.jeus.domain.posts;
 
-import lombok.AllArgsConstructor;
+import jp.co.jeus.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- jeus-todo/src/test/java/jp/co/jeus/domain/posts/PostsRepositoryTest.java
+++ jeus-todo/src/test/java/jp/co/jeus/domain/posts/PostsRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +42,26 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void regist_baseTimeEntity() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2021, 11, 1, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> list = postsRepository.findAll();
+
+        // then
+        Posts posts = list.get(0);
+
+        System.out.println(">>>>>>>>>>> createDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
