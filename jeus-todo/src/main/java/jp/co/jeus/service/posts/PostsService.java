6856aba7112c6a2cc7741e352c9e78bc694198e8
sambunarayan <soyou30257@gmail.com>
2021-11-05 08:38:25 +0900

    20211105 SpringBoot

--- jeus-todo/src/main/java/jp/co/jeus/service/posts/PostsService.java
+++ jeus-todo/src/main/java/jp/co/jeus/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package jp.co.jeus.service.posts;
 
 import jp.co.jeus.domain.posts.Posts;
 import jp.co.jeus.domain.posts.PostsRepository;
+import jp.co.jeus.web.dto.PostListResponseDto;
 import jp.co.jeus.web.dto.PostsResponseDto;
 import jp.co.jeus.web.dto.PostsSaveRequestDto;
 import jp.co.jeus.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("No result record. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
