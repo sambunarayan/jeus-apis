a3b2ac552c25896bcdc0c7724be6861adaba6844
sambunarayan <soyou30257@gmail.com>
2021-11-08 23:15:11 +0900

    20211108 SpringBoot
    ・ArgumentResolver

--- jeus-todo/src/main/java/jp/co/jeus/web/IndexController.java
+++ jeus-todo/src/main/java/jp/co/jeus/web/IndexController.java
@@ -1,5 +1,6 @@
 package jp.co.jeus.web;
 
+import jp.co.jeus.config.auth.LoginUser;
 import jp.co.jeus.config.auth.dto.SessionUser;
 import jp.co.jeus.service.posts.PostsService;
 import jp.co.jeus.web.dto.PostsResponseDto;
@@ -9,20 +10,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
@@ -30,8 +27,7 @@ public class IndexController {
     }
 
     @GetMapping("/posts/save")
-    public String postSave(Model model) {
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+    public String postSave(Model model, @LoginUser SessionUser user) {
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
